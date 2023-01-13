72d55229515baa46d386717f980707983d259ed8
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-01-13 15:32:35 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,36 @@ var { graphqlHTTP } = require('express-graphql');
 const { buildSchema, graphql } = require("graphql");
 
 const schema = buildSchema(`
+type Person{ 
+  name:String
+  email:String
+}
+type Developer{  
+  profile:Person,
+  experience:Int
+}
 type Query{
-     name:String
-     email:String
+     employee:Developer,  
+     isDeveloper:Boolean
 }`);
 
 const resolver = {
-    name: () => {
-        return "hridoy";
-    },
-    email: () => {
-        return "hridoymahmud71@gmail.com";
-    }
+  name: () => {
+    return "hridoy";
+  },
+  email: () => {
+    return "hridoymahmud71@gmail.com";
+  },
+  employee: () => {
+    return { 
+      profile: { 
+        name: "Mahmud",
+        email: "hrim11@gmail.com"
+       },
+       experience: 6 
+      };
+  }
+  
 }
 
 var app = express();
@@ -24,7 +42,7 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql'); 
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
 // graphql({schema, source:"{name,email}",resolver}).then((res) => {
 //   console.log(res);
